e7a795232b48d1126f0edbd09e1f49f29267fcfb
Marcelo Barros <mhbarros99@gmail.com>
2025-12-25 18:56:08 -0300

    feat: add swagger + prettier config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Boilerplate')
+    .setDescription('The perfect boilerplate to start NestJS projects')
+    .setVersion('1.0')
+    .addTag('boilerplate')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();

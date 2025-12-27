03471705aa99e9f053d3d2a68ae2a1b52cdc0456
Marcelo Barros <mhbarros99@gmail.com>
2025-12-27 17:23:45 -0300

    feat: fastify

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Req, Res } from '@nestjs/common'
 import { AppService } from './app.service'
+import type { FastifyReply, FastifyRequest } from 'fastify'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Res() response: FastifyReply, @Req() request: FastifyRequest): string {
+    response.status(302).redirect('/api')
     return this.appService.getHello()
   }
 }
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,9 +2,10 @@ import './instrument'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
 
   const config = new DocumentBuilder()
     .setTitle('NestJS Boilerplate')

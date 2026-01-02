108f50757592dc8d39b9f60cbf04deb1aa54a4a1
Marcelo Barros <mhbarros99@gmail.com>
2026-01-02 20:50:09 -0300

    feat: organization

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,14 +7,20 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
 
+  const SWAGGER_TITLE = 'NestJS Boilerplate'
+  const SWAGGER_DESCRIPTION = 'The perfect boilerplate to start NestJS projects'
+  const SWAGGER_VERSION = '1.0'
+  const SWAGGER_TAG = 'boilerplate'
+  const SWAGGER_ROUTE = 'api'
+
   const config = new DocumentBuilder()
-    .setTitle('NestJS Boilerplate')
-    .setDescription('The perfect boilerplate to start NestJS projects')
-    .setVersion('1.0')
-    .addTag('boilerplate')
+    .setTitle(SWAGGER_TITLE)
+    .setDescription(SWAGGER_DESCRIPTION)
+    .setVersion(SWAGGER_VERSION)
+    .addTag(SWAGGER_TAG)
     .build()
   const documentFactory = () => SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, documentFactory)
+  SwaggerModule.setup(SWAGGER_ROUTE, app, documentFactory)
 
   await app.listen(process.env.PORT ?? 3000)
 }
